3722b1657bb94af6db01db2fa47a2272b95b679a
Keeley Peck <peck.keeley@gmail.com>
2016-10-15 18:52:38 -0400

    completed Q5, but counter is including single space in number of words

---
+++ CSCIE3ProjectUnit2A/js/hw2WordCount.js
@@ -0,0 +1,28 @@
+/* CSCI E-3 Introduction to Web Programming Using Javascript
+ * Spring 2014
+ *
+ * Homework Unit #2
+ *
+ *
+ */
+
+
+ /********************************************************************
+  *
+  * Fifth problem: Word Counter
+  *
+  ********************************************************************/
+// first we'll get the HTMLElement objects for the 
+// textarea and the div where we want to write the
+// output
+var textareaElement = document.getElementById("myWordsToCount");
+
+// now we'll assign a function to the 'onkeyup' handler for the
+// textarea element. This function gets run every time the user
+// completes a keystroke in the textarea.
+textareaElement.onkeyup = function(){
+    var inputString = textareaElement.value;
+    var reduceWhitespace = inputString.replace(/\s+/g, " ");
+    var words = reduceWhitespace.split(" ");
+    document.getElementById("wordcount").innerHTML = words.length;
+}
