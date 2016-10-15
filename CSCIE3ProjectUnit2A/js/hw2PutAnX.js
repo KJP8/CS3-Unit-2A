7682f390a43a36622c24bb5bb5c6ba3237739e41
Keeley Peck <peck.keeley@gmail.com>
2016-10-15 13:19:30 -0400

    cleaned up code, added comments

--- CSCIE3ProjectUnit2A/js/hw2PutAnX.js
+++ CSCIE3ProjectUnit2A/js/hw2PutAnX.js
@@ -16,23 +16,23 @@
 var putAnXBtn = document.getElementById("putAnXButton");
 putAnXBtn.onclick = function(){
 
-// get a reference to the box
-var theBox = document.getElementById("putAnX");
-
-// now get the width and height of the box - see the clientWidth and clientHeight documentation at http://docs.webplatform.org/wiki/dom/HTMLElement
-var width = theBox.clientWidth;
-var height = theBox.clientHeight;
-
-// Generate a random number for the x and y positions of the X
-// and round down to the nearest integer generated
-var xPosition = Math.floor(Math.random() * width);
-var yPosition = Math.floor(Math.random() * height); 
-
-
-//now we'll get the HTML element where the X goes, fill in the X and set the position of the element.
-var theXElement = document.getElementById("theX");
-theXElement.innerHTML="X";
-theXElement.style.top = parseInt(yPosition)+'px';
-theXElement.style.left = parseInt(xPosition)+'px';
+    // get a reference to the box
+    var theBox = document.getElementById("putAnX");
+    
+    // now get the width and height of the box - see the clientWidth and clientHeight documentation at http://docs.webplatform.org/wiki/dom/HTMLElement
+    var width = theBox.clientWidth;
+    var height = theBox.clientHeight;
+    
+    // Generate a random number for the x and y positions of the X
+    // and round down to the nearest integer generated
+    var xPosition = Math.floor(Math.random() * width);
+    var yPosition = Math.floor(Math.random() * height); 
+    
+    
+    //now we'll get the HTML element where the X goes, fill in the X and set the position of the element.
+    var theXElement = document.getElementById("theX");
+    theXElement.innerHTML="X";
+    theXElement.style.top = parseInt(yPosition)+'px';
+    theXElement.style.left = parseInt(xPosition)+'px';
 
  }
--- CSCIE3ProjectUnit2A/js/hw2SplitString.js
+++ CSCIE3ProjectUnit2A/js/hw2SplitString.js
@@ -19,30 +19,17 @@ splitName.onclick = function(){
 
     var fullname = document.getElementById("fullName").value;
     
-    /*
-     *  We've gotten the fullname from the HTML form field.
-     *  Your job is to use the String.slice(), String.substring() or String.substr() functions
-     *  to divide your name into separate first and last name strings and assign
-     *  them to the variables provided. You may
-     *  need String.indexOf() as well.
-     *
-     *  You may not hardcode the position of where to split the string. Your code should
-     *  work for anyone's first and last name. If the user enters a name without any whitespace
-     *  return that as firstname and leave lastname blank.
-     *
-     *  If a name has more than one whitespace (as in, using one or more middle names),
-     *  make the first name 'firstname' and assign the rest to 'lastname'
-     *
-     **/
-    
+    // initialize variables to be used in conditional statements
     var whitespace = fullname.indexOf(" ");
     var firstname;
     var lastname;
     
+    // check for whitespace then use first instance of whitespace as indicator to divide input name into separate first and last name strings
     if (fullname.indexOf(" ") > 0) {
         firstname = fullname.slice(0, whitespace);
         lastname = fullname.slice(whitespace, fullname.length);
     }
+    // if input does not have any whitespace, set the entire user input to the firstname
     else {
         firstname = fullname;
         lastname = "";
@@ -50,6 +37,4 @@ splitName.onclick = function(){
     
     document.getElementById("firstname").innerHTML = firstname;
     document.getElementById("lastname").innerHTML = lastname;
-    
-    
 }
