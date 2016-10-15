1455b2e2e0bcf95e1c1f6d29384d6a4e7151113d
Keeley Peck <peck.keeley@gmail.com>
2016-10-15 13:12:12 -0400

    completed problem 3

--- CSCIE3ProjectUnit2A/js/hw2SplitString.js
+++ CSCIE3ProjectUnit2A/js/hw2SplitString.js
@@ -14,33 +14,42 @@
   ********************************************************************/
 
 
-  var splitName = document.getElementById("splitName");
+var splitName = document.getElementById("splitName");
 splitName.onclick = function(){
 
-               var fullname = document.getElementById("fullName").value;
-
-               /*
-                *  We've gotten the fullname from the HTML form field.
-                *  Your job is to use the String.slice(), String.substring() or String.substr() functions
-                *  to divide your name into separate first and last name strings and assign
-                *  them to the variables provided. You may
-                *  need String.indexOf() as well.
-                *
-                *  You may not hardcode the position of where to split the string. Your code should
-                *  work for anyone's first and last name. If the user enters a name without any whitespace
-                *  return that as firstname and leave lastname blank.
-                *
-                *  If a name has more than one whitespace (as in, using one or more middle names),
-                *  make the first name 'firstname' and assign the rest to 'lastname'
-                *
-                **/
-
-               var firstname = 'Placeholder first name';
-               var lastname = 'Placeholder last name';
-
-
-               document.getElementById("firstname").innerHTML = firstname;
-               document.getElementById("lastname").innerHTML = lastname;
-
-
+    var fullname = document.getElementById("fullName").value;
+    
+    /*
+     *  We've gotten the fullname from the HTML form field.
+     *  Your job is to use the String.slice(), String.substring() or String.substr() functions
+     *  to divide your name into separate first and last name strings and assign
+     *  them to the variables provided. You may
+     *  need String.indexOf() as well.
+     *
+     *  You may not hardcode the position of where to split the string. Your code should
+     *  work for anyone's first and last name. If the user enters a name without any whitespace
+     *  return that as firstname and leave lastname blank.
+     *
+     *  If a name has more than one whitespace (as in, using one or more middle names),
+     *  make the first name 'firstname' and assign the rest to 'lastname'
+     *
+     **/
+    
+    var whitespace = fullname.indexOf(" ");
+    var firstname;
+    var lastname;
+    
+    if (fullname.indexOf(" ") > 0) {
+        firstname = fullname.slice(0, whitespace);
+        lastname = fullname.slice(whitespace, fullname.length);
+    }
+    else {
+        firstname = fullname;
+        lastname = "";
+    }
+    
+    document.getElementById("firstname").innerHTML = firstname;
+    document.getElementById("lastname").innerHTML = lastname;
+    
+    
 }
